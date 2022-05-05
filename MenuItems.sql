2f024b5fcb7121e7fbb6d39994b5c17134904aa0
David-Briffa <94389122+David-Briffa@users.noreply.github.com>
2022-05-05 19:33:14 +0200

    Delete MenuItems.sql

--- MenuItems.sql
+++
@@ -1,5 +0,0 @@
-SELECT * FROM menu.menuitems WHERE Type = "Starter";
-SELECT * FROM menu.menuitems WHERE Type = "Pasta";
-SELECT * FROM menu.menuitems WHERE Type = "Main";
-SELECT * FROM menu.menuitems WHERE Type = "Dessert";
-SELECT * FROM menu.menuitems WHERE ID  = 6;
