6857007abfeb344c9e78c667f07135b07a8298eb
David-Briffa <94389122+David-Briffa@users.noreply.github.com>
2022-05-08 11:00:02 +0200

    fixed session

--- menu_descriptions.php
+++ menu_descriptions.php
@@ -1,88 +1,83 @@
-<?php
+<?php 
 require_once 'db-inc.php';
-
+require 'session.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Details</title>
     <link rel="stylesheet" href="./Styles/MenuStyle.css">
-    <link rel="stylesheet" href="Styles/heFoStyleWhite.css">
 </head>
-
 <body>
-    <?php include "header.html" ?>
+<?php include "header.html" ?>
 
-    <?php
+<?php
+$favID = $_GET['pid'];
 
-    session_start();
-
-
-
-    if (array_key_exists('setFavourite', $_POST)) {
-        setFavourite();
+if(array_key_exists('setFavourite', $_POST)) {
+    setFavourite();
+}
+function setFavourite(){  
+    global $favID;
+    if(!isset($_SESSION["favourite"])){
+        $favArray = array();
+        $_SESSION["favourite"] = $favArray;
     }
-    function setFavourite()
-    {
-        $favID = $_GET['pid'];
-        if (!isset($_SESSION["favourite"])) {
-            $favArray = array();
-            $_SESSION["favourite"] = $favArray;
-        } else {
-            array_push($_SESSION["favourite"], $favID);
-
-            echo "<div class='container'>";
-            echo "favourite dish has been set<br>";
-            echo "</div>";
-        }
+    else{
+    array_push($_SESSION["favourite"], $favID);
+    
+    echo "<div class='container'>";
+        echo "favourite dish has been set<br>";
+    echo "</div>";
     }
+}
 
 
-    if (isset($_GET['pid'])) {
+    if(isset($_GET['pid'])) { 
         $pid = $_GET['pid'];
         $sqlquery = "SELECT * FROM menu.menuitems WHERE ID = $pid;";
         $queryResult = mysqli_query($conn, $sqlquery);
         $resultCheck = mysqli_num_rows($queryResult);
-        if ($resultCheck > 0) {
+        if($resultCheck > 0){
             echo "<div class='container'>";
-            while ($row = mysqli_fetch_assoc($queryResult)) {
+            while($row = mysqli_fetch_assoc($queryResult)){
                 echo "<div class='menu'><br>";
                 echo "<br><br>";
-                echo $row['Name'] . " ";
-                echo $row['Price'] . "€" . "<br>";
-                echo "<div class='description'>";
-                echo $row['Description'] . "<br>";
-                echo "<div class='favourite'>";
-                echo "<br><br>";
-                echo "Like this dish? Favourite it! &#8594;";
-                echo "<form method='post'>";
-                echo "<input type='submit' name='setFavourite' class='favbutton' value=' &hearts; ' />" . "<br><br>";
-                echo " </form>";
-
-                echo "<form action='favourites.php' method='post'>";
-                echo "<input type='submit' name='goToFavourites' class='favbutton' value='Go to favourites' />";
-                echo " </form>";
+                    echo $row['Name']." ";
+                    echo $row['Price']."€". "<br>";
+                        echo "<div class='description'>";
+                            echo $row['Description'] . "<br>";
+                            echo "<div class='favourite'>";
+                                echo "<br><br>";
+                                echo "Like this dish? Favourite it! &#8594;";
+                                echo "<form method='post'>";
+                                    echo "<input type='submit' name='setFavourite' class='favbutton' value=' &hearts; ' />" . "<br><br>";
+                                echo " </form>";
+                                echo "<br><br>";
+                                echo "<form action='favourites.php' method='post'>";
+                                    echo "<input type='submit' name='goToFavourites' class='favbutton' value='Go to favourites' />";
+                                echo " </form>";
+                            echo "</div>";
+                        echo "</div>";
                 echo "</div>";
-                echo "</div>";
-                echo "</div>";
-            }
-            echo "</div>";
         }
+        echo "</div>";
     }
+    
+
+}
 
 
 
-    ?>
-    <div class="divider">
-        SupperThyme
-    </div>
-    <?php include "footer.html" ?>
+?>
+<div class="divider">
+    Restaurant
+</div>
+<?php include "footer.html" ?>
 
 
 </body>
-
 </html>
