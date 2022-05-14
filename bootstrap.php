d05ab068f2d16556b9ad5cd32a4267c758717e7b
Andrea Avona <andrea.avona02@gmail.com>
2022-05-14 23:20:59 +0200

    Did some fixtures to the twig versions
    Fixed booking form that had some conflicting css

--- bootstrap.php
+++ bootstrap.php
@@ -1,10 +1,11 @@
 <?php
 
 // Load our autoloader
-require_once __DIR__.'/vendor/autoload.php';
+//require_once __DIR__ . '/vendor/autoload.php';
+require_once '/Users/andre/vendor/autoload.php';
 
 // Specify our Twig templates location
-$loader = new \Twig\Loader\FilesystemLoader(__DIR__.'/templates');
+$loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/templates');
 
 // Instantiate our Twig. 
 $twig = new \Twig\Environment($loader);
--- db-inc.php
+++ db-inc.php
@@ -1,38 +1,42 @@
-<?php 
-class Db {
+<?php
+class Db
+{
     private static $connection;
-    
-    public function connect() {
-        
-        if(!isset(self::$connection)) {
 
-            $config = parse_ini_file('./config.ini'); 
-            self::$connection = new mysqli($config['server'],$config['username'],$config['password'],$config['dbname']);
+    public function connect()
+    {
+
+        if (!isset(self::$connection)) {
+
+            $config = parse_ini_file('./config.ini');
+            self::$connection = new mysqli($config['server'], $config['username'], $config['password'], $config['dbname']);
         }
 
-        if(self::$connection === false) {
+        if (self::$connection === false) {
             return false;
         }
         return self::$connection;
     }
 
-    public function query($query) {
+    public function query($query)
+    {
         // Connect to the database
-        $connection = $this -> connect();
-        
+        $connection = $this->connect();
+
         // Query the database
-        $result = $connection -> query($query);
-        
+        $result = $connection->query($query);
+
         return $result;
     }
 
-    public function select($query) {
+    public function select($query)
+    {
         $rows = array();
-        $result = $this -> query($query);
-        if($result === false) {
+        $result = $this->query($query);
+        if ($result === false) {
             return false;
         }
-        while ($row = $result -> fetch_assoc()) {
+        while ($row = $result->fetch_assoc()) {
             $rows[] = $row;
         }
         return $rows;
@@ -44,19 +48,21 @@ class Db {
             $stmt->bind_param("s", $sessionId);
             $stmt->execute();
             $stmt->close();
- 
+
             return TRUE;
         } catch (Exception $e) {
             return FALSE;
         }
     }
-    public function error() {
-        $connection = $this -> connect();
-        return $connection -> error;
+    public function error()
+    {
+        $connection = $this->connect();
+        return $connection->error;
     }
 
-    public function quote($value) {
-        $connection = $this -> connect();
-        return "'" . $connection -> real_escape_string($value) . "'";
+    public function quote($value)
+    {
+        $connection = $this->connect();
+        return "'" . $connection->real_escape_string($value) . "'";
     }
 }
