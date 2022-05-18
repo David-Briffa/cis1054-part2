af047160ec2d8a9fab94a4e97d2ee1a4e93797a7
David-Briffa <94389122+David-Briffa@users.noreply.github.com>
2022-05-18 22:19:07 +0200

    Fixed mysqli bug, fully functional
    IT LIVES, IT LIVES!!

--- favourites.php
+++ favourites.php
@@ -5,36 +5,42 @@ require_once __DIR__.'/session.php';
 
 if(array_key_exists('sendEmail', $_POST)) { sendEmail(); }
     function sendEmail(){
-        if(isset($_POST["from"]) && isset($_POST["email"])) {     
-            $to = $_POST['email'];
+
+        if(isset($_POST["from"]) && isset($_POST["to"])) {     
+            $to = $_POST['to'];
             $from = $_POST['from'];
-            $subject = "Check out my favourites!";
-            $headers = "MIME-Version: 1.0" . "\r\n";
-            $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-            $headers .= 'From: <info@example.com>' . "\r\n";
+            $subject = "Check out my favourites!";           
+            $headers = 'From:' . $from;
             $message = "test email";
+
             if(mail($to, $subject, $message, $headers)){ 
-                echo "<div class='container'>";
                 echo 'Email has been sent successfully.';
-                echo "</div>"; 
              }else{ 
                 echo 'Email sending failed.'; 
              }
-        }
+            }
     }
 
 if(array_key_exists('removeFavourite', $_POST)) { removeFavourite(); }
 function removeFavourite(){
+  if (sizeof($_SESSION["favourite"]) == 1){
     unset($_SESSION["favourite"]);
+  }
+    elseif (isset($_SESSION["favourite"])){
+        $key = $_REQUEST['rmv'];
+        $index = array_search($key,$_SESSION["favourite"]);
+        unset($_SESSION["favourite"][$index]);  
+      }
+    }
 
-}
+
 
 
 if(isset($_SESSION["favourite"])){ 
     $db = new Db(); 
     $implodedArray = implode(", ",$_SESSION["favourite"]);
        
-    $result = $db -> select("SELECT Name, Description, Price, Type FROM menuitems WHERE menuitems.ID IN (" . $implodedArray . ")");
+    $result = $db -> select("SELECT ID, Name, Description, Price, Type FROM menuitems WHERE menuitems.ID IN (" . $implodedArray . ")");
     if(count($result) > 0){
     echo $twig->render('favourites.html', ['menuitems' => $result] );
     }
