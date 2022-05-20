1d2efcbc259da7754a96c6009ef48053bb3356a3
David-Briffa <94389122+David-Briffa@users.noreply.github.com>
2022-05-20 16:44:00 +0200

    Cleaning and commenting

--- favourites.php
+++ favourites.php
@@ -3,50 +3,33 @@ require_once __DIR__.'/bootstrap.php';
 require_once __DIR__.'/db-inc.php';
 require_once __DIR__.'/session.php';
 
-if(array_key_exists('sendEmail', $_POST)) { sendEmail(); }
-    function sendEmail(){
-
-        if(isset($_POST["from"]) && isset($_POST["to"])) {     
-            $to = $_POST['to'];
-            $from = $_POST['from'];
-            $subject = "Check out my favourites!";           
-            $headers = 'From:' . $from;
-            $message = "test email";
-
-            if(mail($to, $subject, $message, $headers)){ 
-                echo 'Email has been sent successfully.';
-             }else{ 
-                echo 'Email sending failed.'; 
-             }
-            }
-    }
-
+//binds the removefavourite button with the function below
 if(array_key_exists('removeFavourite', $_POST)) { removeFavourite(); }
+
+//since this session is an array, a key is used to find the index which is then used to remove the appropriate items
 function removeFavourite(){
-  if (sizeof($_SESSION["favourite"]) == 1){
-    unset($_SESSION["favourite"]);
-  }
-    elseif (isset($_SESSION["favourite"])){
-        $key = $_REQUEST['rmv'];
-        $index = array_search($key,$_SESSION["favourite"]);
-        unset($_SESSION["favourite"][$index]);  
-      }
+    //when there is only one item in favourites, unsets the whole thing to avoid an array indexing error
+    if (sizeof($_SESSION["favourite"]) == 1){ 
+        unset($_SESSION["favourite"]);
+    }
+        elseif (isset($_SESSION["favourite"])){
+            $key = $_REQUEST['rmv'];
+            $index = array_search($key,$_SESSION["favourite"]);
+            unset($_SESSION["favourite"][$index]);  
+        }
     }
 
+    if(isset($_SESSION["favourite"])){ 
+        $db = new Db(); 
+        $implodedArray = implode(", ",$_SESSION["favourite"]);  //splits the array into numbers that can be used in the query below   
+        $result = $db -> select("SELECT ID, Name, Description, Price, Type FROM menuitems WHERE menuitems.ID IN (" . $implodedArray . ")");
 
-
-
-if(isset($_SESSION["favourite"])){ 
-    $db = new Db(); 
-    $implodedArray = implode(", ",$_SESSION["favourite"]);
-       
-    $result = $db -> select("SELECT ID, Name, Description, Price, Type FROM menuitems WHERE menuitems.ID IN (" . $implodedArray . ")");
-    if(count($result) > 0){
-    echo $twig->render('favourites.html', ['menuitems' => $result] );
+        if(count($result) > 0){
+        echo $twig->render('favourites.html', ['menuitems' => $result] );
     }
-
-}else 
-echo $twig->render('noFavourites.html');   
+    }else 
+        echo $twig->render('noFavourites.html');   
+        //echoes out the noFavourites page if the session array is empty
 
 
 
--- menu_details.php
+++ menu_details.php
@@ -10,16 +10,15 @@ $favArray = array();
 if(array_key_exists('setFavourite', $_POST)) {
     setFavourite();
 }
+//pushes the item onto the favourites array 
 function setFavourite(){  
     global $favID;
     global $favArray;
     if(!isset($_SESSION["favourite"])){
-        
         $_SESSION["favourite"] = $favArray;
         array_push($_SESSION["favourite"], $favID);
-
     }
-    else{   array_push($_SESSION["favourite"], $favID); }
+    else{   array_push($_SESSION["favourite"], $favID);  }
 }
 
 if(isset($_GET['pid'])) 
@@ -27,12 +26,13 @@ if(isset($_GET['pid']))
     $db = new Db();    
     $foodID = $db -> quote($_GET['pid']);
     $result = $db -> select("SELECT Name, Description, Price, Type FROM menuitems WHERE ID=". $foodID);
-
+//displays the item associated with the button pressed on the menu page
     if (count($result) > 0){
         echo $twig->render('details.html', ['menuitems' => $result] );
     }
     else
         echo $twig->render('404.html');
-}
+}       //no items in database, no page
 else
     echo $twig->render('404.html');
+    //no pid, no page
